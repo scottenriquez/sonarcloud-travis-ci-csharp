4a866bf1334961cd0d84bc37c570e0775d1625c7
Scottie Enriquez <scott.c.enriquez@gmail.com>
2019-05-18 13:09:30 -0500

    Removing deprecated .exe references in build script

--- tools/travis-ci-build.sh
+++ tools/travis-ci-build.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
+echo "Changing to /src directory..."
 cd src
-mono ../tools/sonar/SonarQube.Scanner.MSBuild.exe begin /k:"scottenriquez_sonarcloud-travis-ci-csharp" /d:sonar.organization="scottenriquez-github" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login=${SONAR_TOKEN}
+echo "Executing MSBuild.exe begin command..."
+mono ../tools/sonar/SonarScanner.MSBuild.exe begin /o:"scottenriquez-github" /k:"scottenriquez_sonarcloud-travis-ci-csharp" /d:sonar.organization="scottenriquez-github" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login=${SONAR_TOKEN}
+echo "Running build..."
 dotnet build
+echo "Running tests..."
 dotnet test SonarCloud.Travis.Integration.Tests/SonarCloud.Travis.Integration.Tests.csproj
-mono ../tools/sonar/SonarQube.Scanner.MSBuild.exe end /d:sonar.login=${SONAR_TOKEN}
+echo "Executing MSBuild.exe end command..."
+mono ../tools/sonar/SonarScanner.MSBuild.exe end /d:sonar.login=${SONAR_TOKEN}

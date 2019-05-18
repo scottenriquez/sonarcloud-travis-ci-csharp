b823c133a2d0fbbc621c945e0e994b0ffc267fb8
Scottie Enriquez <scott.c.enriquez@gmail.com>
2019-05-18 12:20:54 -0500

    Downgrading SonarScanner for MSBuild to version 4.0.2.892

--- tools/travis-ci-install-sonar.sh
+++ tools/travis-ci-install-sonar.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 echo "Starting install..."
-wget -O sonar.zip https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/4.6.1.2049/sonar-scanner-msbuild-4.6.1.2049-netcoreapp2.0.zip
+wget -O sonar.zip https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/4.0.2.892/sonar-scanner-msbuild-4.0.2.892.zip
 echo "Unzipping..."
 unzip -qq sonar.zip -d tools/sonar
 echo "Displaying file structure..."
 find .
-echo $(find .)
 ls -l tools/sonar
 echo "Changing permissions..."
-chmod +x tools/sonar/sonar-scanner-3.3.0.1492/bin/sonar-scanner
+chmod +x tools/sonar/sonar-scanner-3.0.3.778/bin/sonar-scanner

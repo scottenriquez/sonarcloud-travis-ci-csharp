f0427c48f683df25d1258369c4b83747a348751a
Scottie Enriquez <scott.c.enriquez@gmail.com>
2019-05-18 13:43:45 -0500

    Using .NET Core's MSBuild wrapper instead of the standard build command in Travis CI build script

--- tools/travis-ci-build.sh
+++ tools/travis-ci-build.sh
@@ -4,7 +4,7 @@ cd src
 echo "Executing MSBuild.exe begin command..."
 mono ../tools/sonar/SonarScanner.MSBuild.exe begin /o:"scottenriquez-github" /k:"scottenriquez_sonarcloud-travis-ci-csharp" /d:sonar.organization="scottenriquez-github" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login=${SONAR_TOKEN}
 echo "Running build..."
-dotnet build
+dotnet msbuild /p:Configuration=Release SonarCloud.Travis.Integration.sln
 echo "Running tests..."
 dotnet test SonarCloud.Travis.Integration.Tests/SonarCloud.Travis.Integration.Tests.csproj
 echo "Executing MSBuild.exe end command..."

8b989755d9662fbb9bb6ee69e8b4bd89c295ffd0
Scottie Enriquez <scott.c.enriquez@gmail.com>
2019-05-19 12:44:16 -0500

    Updating .gitignore; fixing organization parameter in SonarScanner for MSBuild command

--- tools/travis-ci-build.sh
+++ tools/travis-ci-build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 echo "Changing to /src directory..."
 cd src
-echo "Executing MSBuild DLL begin command..."
-dotnet ../tools/sonar/SonarScanner.MSBuild.dll begin /o:"scottenriquez-github" /k:"scottenriquez_sonarcloud-travis-ci-csharp" /d:sonar.organization="scottenriquez-github" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login=${SONAR_TOKEN}
+echo "Executing MSBuild.exe begin command..."
+mono ../tools/sonar/SonarScanner.MSBuild.exe begin /o:"scottenriquez-github" /k:"scottenriquez_sonarcloud-travis-ci-csharp" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.verbose=true /d:sonar.login=${SONAR_TOKEN}
 echo "Running build..."
-dotnet build SonarCloud.Travis.Integration.sln
+msbuild /p:Configuration=Release /t:Rebuild SonarCloud.Travis.Integration.sln
 echo "Running tests..."
 dotnet test SonarCloud.Travis.Integration.Tests/SonarCloud.Travis.Integration.Tests.csproj
 echo "Executing MSBuild.exe end command..."
-dotnet ../tools/sonar/SonarScanner.MSBuild.dll end /d:sonar.login=${SONAR_TOKEN}
+mono ../tools/sonar/SonarScanner.MSBuild.exe end /d:sonar.login=${SONAR_TOKEN}

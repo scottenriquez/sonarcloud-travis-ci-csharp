51f15c874e4e7e320e3672f0b829d6e973ea16d2
Scottie Enriquez <scott.c.enriquez@gmail.com>
2019-05-20 18:20:05 -0500

    Fixing Mono command in .NET Core build script

--- tools/travis-ci-build.sh
+++ tools/travis-ci-build.sh
@@ -7,5 +7,5 @@ echo "Running build..."
 dotnet build SonarCloud.Travis.Integration.sln
 echo "Running tests..."
 dotnet test SonarCloud.Travis.Integration.Tests/SonarCloud.Travis.Integration.Tests.csproj
-echo "Executing MSBuild.exe end command..."
-mono ../tools/sonar/SonarScanner.MSBuild.exe end /d:sonar.login=${SONAR_TOKEN}
+echo "Executing MSBuild DLL end command..."
+dotnet ../tools/sonar/SonarScanner.MSBuild.dll end /d:sonar.login=${SONAR_TOKEN}

4cf23f8747d12c38f74c2f93b0325010e4b4de49
Scottie Enriquez <scott.c.enriquez@gmail.com>
2019-05-20 17:48:33 -0500

    Installing and using .NET Core SonarScanner build

--- tools/travis-ci-build.sh
+++ tools/travis-ci-build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 echo "Changing to /src directory..."
 cd src
-echo "Executing MSBuild.exe begin command..."
-mono ../tools/sonar/SonarScanner.MSBuild.exe begin /o:"scottenriquez-github" /k:"scottenriquez_sonarcloud-travis-ci-csharp" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.verbose=true /d:sonar.login=${SONAR_TOKEN}
+echo "Executing MSBuild DLL begin command..."
+dotnet ../tools/sonar/SonarScanner.MSBuild.dll begin /o:"scottenriquez-github" /k:"scottenriquez_sonarcloud-travis-ci-csharp" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.verbose=true /d:sonar.login=${SONAR_TOKEN}
 echo "Running build..."
-msbuild /p:Configuration=Release /t:Rebuild SonarCloud.Travis.Integration.sln
+dotnet build SonarCloud.Travis.Integration.sln
 echo "Running tests..."
 dotnet test SonarCloud.Travis.Integration.Tests/SonarCloud.Travis.Integration.Tests.csproj
 echo "Executing MSBuild.exe end command..."
--- tools/travis-ci-install-sonar.sh
+++ tools/travis-ci-install-sonar.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Starting install..."
-wget -O sonar.zip https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/4.6.1.2049/sonar-scanner-msbuild-4.6.1.2049-net46.zip
+wget -O sonar.zip https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/4.6.1.2049/sonar-scanner-msbuild-4.6.1.2049-netcoreapp2.0.zip
 echo "Unzipping..."
 unzip -qq sonar.zip -d tools/sonar
 echo "Displaying file structure..."
